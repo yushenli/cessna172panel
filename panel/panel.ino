e14e781ed15639bcfa296950a73c3b04a596a047
loudly <yushenli@users.noreply.github.com>
2020-05-11 01:04:12 +0100

    Attempt to debounce by accepting PIN state changes only after the new value has been observed multiple times in a row.

--- panel/panel.ino
+++ panel/panel.ino
@@ -2,7 +2,11 @@
 
 #define UPDATE_STATE_LENGTH_US             2000000
 #define CHECK_INTERVAL_US_UPDATE_STATE         100
-#define CHECK_INTERVAL_US_NON_UPDATE_STATE    5000  // 5 miliseconds
+#define CHECK_INTERVAL_US_NON_UPDATE_STATE    1000  // 5 miliseconds
+
+// Only if the input from CLK pin remains the same value STABLE_LENGTH times in a row,
+// before it is considered the new CLK value.
+#define STABLE_LENGTH            10
 
 #define ALTIMETER                0
 #define PIN_ALTIMETER_CLK        23
@@ -13,11 +17,17 @@ class Knob {
         Knob(const char* knobName, int pinClk, int pinDt, int pulseDegree);
         bool Update();
     private:
+        int updateAndGetLastClk(int newClk);
+        int updateAndGetLastDt(int newDt);
         const char* knobName;
         int pinClk;
         int pinDt;
         int pulseDegree;  // Number of degrees turned for one pulse
-        int lastClk;
+        int lastClkIndex = 0;  // The index where lastClk was last updated in the previous loop.
+        int lastClk[STABLE_LENGTH];
+        int lastStableClk;
+        int lastDtIndex = 0;  // The index where lastClk was last updated in the previous loop.
+        int lastDt[STABLE_LENGTH];
         int degree;
 };
 
@@ -29,7 +39,10 @@ Knob::Knob(const char* knobName, int pinClk, int pinDt, int pulseDegree) {
 
     pinMode(this->pinClk, INPUT);
     pinMode(this->pinDt, INPUT);
-    this->lastClk = digitalRead(this->pinClk);
+    lastStableClk = digitalRead(this->pinClk);
+    for (int i = 0; i < STABLE_LENGTH; i++) {
+        lastClk[i] = lastStableClk;
+    }
     this->degree = 0;
 
     Serial.print("Initialized knob ");
@@ -41,15 +54,17 @@ Knob::Knob(const char* knobName, int pinClk, int pinDt, int pulseDegree) {
 }
 
 bool Knob::Update() {
-    int valClk = digitalRead(pinClk);
-    int valDt = digitalRead(pinDt);
-    if (valClk != lastClk) {
+    int valClk = updateAndGetLastClk(digitalRead(pinClk));
+    int valDt = updateAndGetLastDt(digitalRead(pinDt));
+    if (valClk == -1 || valDt == -1) {
+        return false;
+    }
+
+    if (valClk != lastStableClk) {
         if (IS_DEBUG) {
             Serial.print("reading pin ");
             Serial.print(pinClk);
-            Serial.print(" last ");
-            Serial.print(lastClk);
-            Serial.print(" now ");
+            Serial.print(" new value ");
             Serial.println(valClk);
         }
 
@@ -66,10 +81,41 @@ bool Knob::Update() {
             degree = (degree - pulseDegree + 360) % 360;
         }
         Serial.println(degree);
-        lastClk = valClk;
+
+        lastStableClk = valClk;
         return true;
     }
     return false;
+}
+
+// Update the lastClk array using the newClk value. If all the recent STABLE_LENGTH values are the same,
+// return that value, otherwise return -1.
+int Knob::updateAndGetLastClk(int newClk) {
+    lastClkIndex = (lastClkIndex + 1) % STABLE_LENGTH;
+    lastClk[lastClkIndex] = newClk;
+    for (int i = (lastClkIndex + STABLE_LENGTH - 1) % STABLE_LENGTH;
+            i != lastClkIndex;
+            i = (i + STABLE_LENGTH - 1) % STABLE_LENGTH) {
+        if (lastClk[i] != newClk) {
+            return -1;
+        }
+    }
+    return newClk;
+}
+
+// Update the lastDt array using the newDt value. If all the recent STABLE_LENGTH values are the same,
+// return that value, otherwise return -1.
+int Knob::updateAndGetLastDt(int newDt) {
+    lastDtIndex = (lastDtIndex + 1) % STABLE_LENGTH;
+    lastDt[lastDtIndex] = newDt;
+    for (int i = (lastDtIndex + STABLE_LENGTH - 1) % STABLE_LENGTH;
+            i != lastDtIndex;
+            i = (i + STABLE_LENGTH - 1) % STABLE_LENGTH) {
+        if (lastDt[i] != newDt) {
+            return -1;
+        }
+    }
+    return newDt;
 }
 
 Knob* knobs[1];
