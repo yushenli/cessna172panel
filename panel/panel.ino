ff092dba2ce7fc19eee9c5f868891478274937ee
yushenli <yushenli@users.noreply.github.com>
2020-05-23 19:43:02 +0100

    Reaplce constants using DEFINE with C++ style constants.

--- panel/panel.ino
+++ panel/panel.ino
@@ -9,21 +9,26 @@
 #define CHECK_INTERVAL_US_UPDATE_STATE         100
 #define CHECK_INTERVAL_US_NON_UPDATE_STATE    5000  // 5 miliseconds
 
-#define HDG_ADJUST                0
-#define PIN_HDG_ADJUST_CLK       53
-#define PIN_HDG_ADJUST_DT        52
-#define HDG_BUG                   1
-#define PIN_HDG_BUG_CLK          51
-#define PIN_HDG_BUG_DT           50
-#define VOR2                      2
-#define PIN_VOR2_CLK             49
-#define PIN_VOR2_DT              48
-#define ALTIMETER                 3
-#define PIN_ALTIMETER_CLK         4
-#define PIN_ALTIMETER_DT          5
-#define VOR1                      4
-#define PIN_VOR1_CLK              2
-#define PIN_VOR1_DT               3
+enum knobIndex {
+    kKnobIndexHeadingAdjust = 0,
+    kKnobIndexHeadingBug    = 1,
+    kKnobIndexVOR2          = 2,
+    kKnobIndexAltimeter     = 3,
+    kKnobIndexVOR1          = 4
+};
+const struct {
+    const char* name;
+    const int pinClk;
+    const int pinDt;
+} kKnobsSpecs[] = {
+    // Lower level knobs:
+    {"HeadingAdjust", 53, 52},
+    {"HeadingBug", 51, 50},
+    {"VOR2", 49, 48},
+    // Upper level knobs:
+    {"Altimeter", 4, 5},
+    {"VOR1", 2, 3}
+};
 
 // You can configure what instruments the aircraft shall have by editting
 // this const array to change.
@@ -32,7 +37,7 @@ const Instrument** (*kCessna172InstrumentGroups[])(int*) = {
     CreateNavigationInstruments
 };
 
-Knob* knobs[5];
+Knob** knobs;
 Instrument** instruments;
 int instrumentsCount;
 InstrumentAdapter* instrumentAdapter;
@@ -66,6 +71,28 @@ void knobToInstrumentCallback(Instrument* p, const char* instrumentName, int kno
 
 // Creates all the Knobs supported by the hardware.
 void initKnobs() {
+    const int count = sizeof(kKnobsSpecs) / sizeof(kKnobsSpecs[0]);
+    knobs = new Knob*[count];
+    /*DEBUG_PRINT("Total number of knobs defined: ");
+    DEBUG_PRINTLN(count);
+    for (int i = 0; i < count; i++) {
+        knobs[i] = new Knob(kKnobsSpecs[i].name, kKnobsSpecs[i].pinClk, kKnobsSpecs[i].pinDt, 12);
+    }*/
+#define HDG_ADJUST                0
+#define PIN_HDG_ADJUST_CLK       53
+#define PIN_HDG_ADJUST_DT        52
+#define HDG_BUG                   1
+#define PIN_HDG_BUG_CLK          51
+#define PIN_HDG_BUG_DT           50
+#define VOR2                      2
+#define PIN_VOR2_CLK             49
+#define PIN_VOR2_DT              48
+#define ALTIMETER                 3
+#define PIN_ALTIMETER_CLK         4
+#define PIN_ALTIMETER_DT          5
+#define VOR1                      4
+#define PIN_VOR1_CLK              2
+#define PIN_VOR1_DT               3
     knobs[HDG_ADJUST] = new Knob("HDG_ADJUST", PIN_HDG_ADJUST_CLK, PIN_HDG_ADJUST_DT, 12);
     knobs[HDG_BUG] = new Knob("HDG_BUG", PIN_HDG_BUG_CLK, PIN_HDG_BUG_DT, 12);
     knobs[VOR2] = new Knob("VOR2", PIN_VOR2_CLK, PIN_VOR2_DT, 12);
@@ -91,6 +118,11 @@ void initInstruments() {
     for (int i = 0; i < numInstrumentGroups; i++) {
         for (int j = 0; j < groupSizes[i]; j++) {
             *p = (instrumentGroups[i])[j];
+            /*knobToInstrumentCallback(*p, "HeadingAdjust", knobs[kKnobIndexHeadingAdjust]);
+            knobToInstrumentCallback(*p, "HeadingBug", knobs[kKnobIndexHeadingBug]);
+            knobToInstrumentCallback(*p, "Altimeter", knobs[kKnobIndexAltimeter]);
+            knobToInstrumentCallback(*p, "VOR1", knobs[kKnobIndexVOR1], true);
+            knobToInstrumentCallback(*p, "VOR2", knobs[kKnobIndexVOR2], true);*/
             knobToInstrumentCallback(*p, "HeadingAdjust", HDG_ADJUST);
             knobToInstrumentCallback(*p, "HeadingBug", HDG_BUG);
             knobToInstrumentCallback(*p, "Altimeter", ALTIMETER);
